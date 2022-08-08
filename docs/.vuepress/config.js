7715da4a91b5a501cd25bd1fcd22ef68c155380e
@ozip.cf <41739417+ozipoetra@users.noreply.github.com>
2022-08-09 06:40:05 +0700

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  title: 'Mediumish',
+  title: 'NekoPay',
   base: '/',
   dest: 'public',
-  description: 'Mediumish theme with VuePress and Forestry',
+  description: 'Another trash and usefulness site',
   logo: './assets/img/logo.png',
   theme: require.resolve('../../'),
   themeConfig: {
   authors: [
       {
-      name: 'Sal',
+      name: 'NekoPay',
       avatar: '/assets/img/sal.jpg',
-      link: 'https://wowthemes.net/donate',
+      link: 'https://instagram.com/ozip.cf',
       linktext: 'Follow',
       },
       {
-        name: 'John Doe',
+        name: 'OZIP',
         avatar: '/assets/img/avatar.png',
-        link: 'https://bootstrapstarter.com/',
+        link: 'https://ozip.cf/',
         linktext: 'Follow',
       },
     ],
@@ -73,24 +73,24 @@ module.exports = {
           link: 'https://bootstrapstarter.com/license/',
         },
         {
-          text: 'Made with Mediumish - free Vuepress theme',
-          link: 'https://bootstrapstarter.com/bootstrap-templates/vuepress-theme-mediumish/',
+          text: 'Made with Mediumish - NekoPay CH',
+          link: '#',
         },
       ],
     },
 
     sitemap: {
-      hostname: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/'
+      hostname: 'https://ozip.my.id/'
     },
     comment: {
       service: 'disqus',
-      shortname: 'demowebsite',
+      shortname: 'nekpaych',
     },
     newsletter: {
       endpoint: 'https://wowthemes.us11.list-manage.com/subscribe/post?u=8aeb20a530e124561927d3bd8&id=8c3d2d214b'
     },
     feed: {
-      canonical_base: 'https://github.com/wowthemesnet/vuepress-theme-mediumish/',
+      canonical_base: 'https://ozip.my.id/',
     },
     smoothScroll: true
   },
